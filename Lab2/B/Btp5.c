8655e35e82231910232aff6a3c14e2cd0b9bee4c
Martinho Gomes Figueiredo <hell51.games@gmail.com>
2022-04-21 17:39:37 +0000

    LAB3 done except 6

--- Lab2/B/Btp5.c
+++ Lab2/B/Btp5.c
@@ -7,6 +7,8 @@
 
 #define BUFFER_MAX_SIZE 4
 
+pthread_mutex_t lock;
+
 unsigned int data_space[BUFFER_MAX_SIZE];
 circ_buff_t buffer = { 
     data_space,
@@ -19,12 +21,15 @@ void* producer(void *arg)
 {
     unsigned int push_value;
     while (1) {
+        pthread_mutex_lock(&lock);
         push_value = (rand() % 1000); //random [0,999]
+        printf("ENTRA PRODUCER\n");
         if (circ_buff_push(&buffer, push_value ) == 0)
             printf("Producer: %u\n", push_value);
         else
             printf("Producer: buffer is full\n"); 
-        
+        printf("SAIU PRODUCER\n");
+        pthread_mutex_unlock(&lock);
         usleep(100*1000); //100 ms
     }
     
@@ -35,11 +40,16 @@ void* consumer(void *arg)
 {
     unsigned int pop_value;
     while (1) {
+        pthread_mutex_lock(&lock);
+        printf("                              ENTRA CONSUMER\n");
+
         if (circ_buff_pop(&buffer, &pop_value)==0)
             printf("                              Consumer: returned %u\n", pop_value);
         else
             printf("                              Consumer: buffer is empty\n");
-
+        
+        pthread_mutex_unlock(&lock);
+        printf("                              SAIU CONSUMER\n");
         usleep(150*1000); //150 ms
     }
     
@@ -51,6 +61,11 @@ int main(void)
 	srand(time(NULL));
 
     pthread_t tid[2];
+
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        printf("\n mutex init has failed\n");
+        return 1;
+    }
     
 	pthread_create(&(tid[0]), NULL, &producer, NULL);
 	pthread_create(&(tid[1]), NULL, &consumer, NULL);
@@ -58,5 +73,6 @@ int main(void)
     pthread_join(tid[0], NULL);
     pthread_join(tid[1], NULL);
 
+    pthread_mutex_destroy(&lock);
     return 0;
 }
